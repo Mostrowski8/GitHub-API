b2f8325c38b774579c4eaefc90559c26e8bc3932
mostrowski8 <mostrowski8@gmail.com>
2019-05-27 08:44:54 +0200

    Updates

---
+++ precache-manifest.f4bf56e33b4b9d8295718125675fa83d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72ff00ef12d54868675b9d38107ddb8a",
+    "url": "/GitHub-API/index.html"
+  },
+  {
+    "revision": "1519c0e2b53ef489b5c7",
+    "url": "/GitHub-API/static/css/main.c9471f98.chunk.css"
+  },
+  {
+    "revision": "97c62e07ed6920be9e79",
+    "url": "/GitHub-API/static/js/2.92bc7e4a.chunk.js"
+  },
+  {
+    "revision": "1519c0e2b53ef489b5c7",
+    "url": "/GitHub-API/static/js/main.b12a71f7.chunk.js"
+  },
+  {
+    "revision": "8cc3574a56a927f6cd52",
+    "url": "/GitHub-API/static/js/runtime~main.006ff2e0.js"
+  }
+]);
